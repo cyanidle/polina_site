2e7ab1743e0cd3f5bc8cbef93c17e439edb77593
Alexej Doronin <lyosha.doronin@gmail.com>
2026-07-09 23:39:19 +0200

    node_modules

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-deno run --allow-net --allow-read --allow-env --allow-write --allow-run=magick,convert,identify "`dirname $0`/server.ts" 0.0.0.0 ${1:-9090}
+
+DIR=$(realpath `dirname $0/..`)
+
+DENO_DIR=$DIR/node_modules deno run \
+    --allow-net --allow-read --allow-env --allow-write \
+    --allow-run=magick,convert,identify \
+    "$DIR/server.ts" 0.0.0.0 ${1:-9090}
