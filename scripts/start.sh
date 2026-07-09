42c0721942f55d54137343c9661b588614706c40
Alexej Doronin <lyosha.doronin@gmail.com>
2026-07-09 23:42:51 +0200

    Vendor

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 DIR=$(realpath `dirname $0/..`)
 
-DENO_DIR=$DIR/node_modules deno run \
+deno run \
     --allow-net --allow-read --allow-env --allow-write \
     --allow-run=magick,convert,identify \
     "$DIR/server.ts" 0.0.0.0 ${1:-9090}
